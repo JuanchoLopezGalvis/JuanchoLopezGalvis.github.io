795c43f543a98f4010e0a3351c066ed417121ff9
Juan Diego Lopez Galvis <jdilopezg@unbosque.edu.co>
2026-08-16 22:39:33 -0500

    Set site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://JuanchoLopezGalvis.github.io",
   devToolbar: {
     enabled: false,
   },
